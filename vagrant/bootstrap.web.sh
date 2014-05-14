c02ced6596aa61442389c556ed1b8c6aac8bfd79
Omni Adams <omni@digitaldarkness.com>
2014-05-14 18:03:06 -0500

    Added Twig dependency and chopped new character form up
    We'll eventuall want to reuse some of the content blocks (for displaying old
    characters, for example). And smaller files are easier to work with. Broke the
    new character form up into several different twig templates.
    For issue #14

--- vagrant/bootstrap.web.sh
+++ vagrant/bootstrap.web.sh
@@ -5,4 +5,7 @@ apt-get install -y apache2 php5 2> /dev/null
 rm -rf /var/www
 ln -fs /vagrant/web /var/www
 cp /vagrant/vagrant/web.apache.conf /etc/apache2/sites-available/default
+mkdir /var/twig-cache
+chmod 700 /var/twig-cache
+chown www-data /var/twig-cache
 service apache2 restart
